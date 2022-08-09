3ef476b95dabdc7dbe26fb9fecfc918476592b17
kozykin.k <panchen.panchen@gmail.com>
2022-08-09 08:27:10 +0300

    test2

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,9 +2,9 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        MusicInstr guitar = new MusicInstr("guitar");
-        guitar.play();
+        MusicInstr guitar = new MusicInstr("труба");
         guitar.sayName();
+        guitar.play();
         guitar.sayType();
     }
 }
--- src/main/java/guru/qa/MusicInstr.java
+++ src/main/java/guru/qa/MusicInstr.java
@@ -8,18 +8,41 @@ public class MusicInstr {
         this.name = name;
     }
     void play() {
-        if (name == "guitar") {
-            System.out.println("брынь-брынь");
-        } else {
-            System.out.println("непонятно");
+        switch (name) {
+            case  ("гитара"):
+                System.out.println("брынь-брынь");
+                break;
+            case ("барабан"):
+                System.out.println("бом-бом");
+                break;
+            case ("труба"):
+                System.out.println("бу-бу-бу");
+                break;
+            default:
+                System.out.println("непонятно");
+                break;
         }
     }
 
     void sayName() {
-        System.out.println("Это " + name);
+        System.out.println("это " + name);
     }
 
     void sayType() {
-
+        System.out.println("тип: ");
+        switch (name) {
+            case  ("гитара"):
+                System.out.println("струнные");
+                break;
+            case ("барабан"):
+                System.out.println("ударные");
+                break;
+            case ("труба"):
+                System.out.println("духовые");
+                break;
+            default:
+                System.out.println("непонятно");
+                break;
+        }
     }
 }
